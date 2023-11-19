6a6c36efa313996d59435419c091443ee770762a
Naod Taddese <naodtds@gmail.com>
2023-11-19 22:46:45 +0300

    fixed unknown instruction

--- handle_op.c
+++ handle_op.c
@@ -11,7 +11,8 @@ int handle_op(FILE *mfile, stack_t **stack)
 	char *command = NULL, *first;
 	size_t size = 0, i = 0;
 	unsigned int line_c = 1;
-	instruction_t co_op[] = {{"push", push_sq}, {"pall", pall_sq}, {"pint", pint_sq}, {NULL, NULL}};
+	instruction_t co_op[] = {{"push", push_sq}, {"pall", pall_sq},
+{"pint", pint_sq}, {NULL, NULL}};
 
 	valie = 0;
 	while (getline(&command, &size, mfile) > 0)
@@ -36,16 +37,15 @@ int handle_op(FILE *mfile, stack_t **stack)
 		}
 		line_c++;
 	}
-	free(command);
 	if (first)
 		if (!co_op[i].opcode)
 		{
 			line_c = line_c - 1;
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_c, first);
-			free(command);
 			fclose(mfile);
 			frees(*stack);
 			exit(EXIT_FAILURE);
 		}
+	free(command);
 	return (0);
 }
